43bf6b4c24749704d1b8886575652b107c4f5465
xidongxu <xi_dongxu@126.com>
2025-04-02 00:07:17 +0800

    修改压缩与解压函数的使用
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- callstack/callstack.cpp
+++ callstack/callstack.cpp
@@ -1,5 +1,5 @@
 ﻿#include "callstack.h"
-
+#include "fastlz.h"
 #include <windows.h>
 #include <dbghelp.h>
 #include <stdio.h>
@@ -19,11 +19,26 @@ void callstack() {
     symbol->MaxNameLen = 255;
     symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
 
-    printf("Call stack:\n");
+    char* buffer = (char*)malloc(sizeof(UINT8) * 256);
     for (unsigned short i = 0; i < frames; i++) {
         SymFromAddr(process, (DWORD64)(stack[i]), 0, symbol);
-        printf("%i: %s - 0x%0llX\n", i, symbol->Name, symbol->Address);
+        snprintf((buffer + i * 15), (256 - i * 15), "0x%0llX ", symbol->Address);
     }
+    int length = strnlen(buffer, 256);
+    printf("compress before :[%d][%s]\n", length, buffer);
+
+    void* compress = malloc(sizeof(UINT8) * 256);
+    memset(compress, 0, 256);
+    int result = fastlz_compress((void *)buffer, strnlen(buffer, 256), compress);
+    printf("compress result :[%d]\n", result);
+
+    void* decompress = malloc(sizeof(UINT8) * 256);
+    memset(decompress, 0, 256);
+    result = fastlz_decompress(compress, result, decompress, 256);
+    printf("decompress result :[%d][%s]\n", result, (char*)decompress);
 
     free(symbol);
+    free(buffer);
+    free(compress);
+    free(decompress);
 }
