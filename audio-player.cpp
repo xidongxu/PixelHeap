2fc9eceb725312105e745029de74d6b719c35e8e
xidongxu <xi_dongxu@126.com>
2025-05-03 23:27:44 +0800

    chore(FreeRTOS-LTS): FreeRTOS-LTS源码
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- audio-player.cpp
+++ audio-player.cpp
@@ -3,10 +3,37 @@
 #include "fastlz.h"
 #include "callstack.h"
 
-int main()
-{
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+#include <timers.h>
+#include <semphr.h>
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName) {
+    (void)xTask;
+    (void)pcTaskName;
+    for (;;);
+}
+
+static void playerTask(void* parameters) {
     callstack();
     main_audio();
     main_player();
+    while(1) {
+        printf("player task tick:%lld\n", xTaskGetTickCount());
+        vTaskDelay(100);
+    }
+}
+
+static void playerTaskInit(void) {
+    static StaticTask_t playerTaskTCB;
+    static StackType_t playerTaskStack[256];
+    printf("FreeRTOS Plyaer Project\n");
+    xTaskCreateStatic(playerTask, "player", 256, NULL, configMAX_PRIORITIES - 1U, &(playerTaskStack[0]), &(playerTaskTCB));
+    vTaskStartScheduler();
+}
+
+int main(void) {
+    playerTaskInit();
     return 0;
 }
